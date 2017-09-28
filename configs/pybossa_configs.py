12fd5df140d42ea7eee31944e3c890d247d77676
Arsenii Zhdanov <arsenzhd@gmail.com>
2017-09-28 17:11:28 +0300

    Fix bug with template and update configs

--- configs/pybossa_configs.py
+++ configs/pybossa_configs.py
@@ -4,8 +4,8 @@ import os
 """
 Pybossa related configs.
 """
-ENDPOINT = os.environ.get('PYBOSSA_ENDPOINT', 'http://ec2-18-194-103-85.eu-central-1.compute.amazonaws.com:5000')
-API_KEY = os.environ.get('PYBOSSA_KEY', 'fd45bdd1-e2e3-45d0-80d4-f17969d61d4f')
+ENDPOINT = os.environ.get('PYBOSSA_ENDPOINT', 'http://ec2-18-194-119-31.eu-central-1.compute.amazonaws.com:5000')
+API_KEY = os.environ.get('PYBOSSA_KEY', 'ba30e8ad-1622-4728-ad59-9659fcbd3e78')
 PROJECT_CONFIGS = os.environ.get('PYBOSSA_PROJECT',
                                  os.path.join(main_configs.BASE_DIR, "pybossa_importer", "project.json"))
 TASKS = os.environ.get('PYBOSSA_TASKS', os.path.join(main_configs.BASE_DIR, "tmp", "reuters.json"))
